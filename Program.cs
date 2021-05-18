cfb0dfb32d3cf63b6ac934ee86c7708702e8cb4d
ST Programming <stcorp@outlook.it>
2021-05-18 12:29:54 +0200

    Add performance to oop system

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace GIocodellaSigaretta
         static void Main(string[] args)
         {
             Game Rules = new Game();
+            Contents gameContents = new Contents();
             Console.WriteLine("Hello World!");
             Console.WriteLine("In quanti siete a giocare? da " + Rules.minPlayer + " a " + Rules.maxPlayer);
             insertNumPlayers();
@@ -32,7 +33,7 @@ namespace GIocodellaSigaretta
                 Console.WriteLine("Mi spiace ma non si può giocare in " + numPlayers + ". Dovete essere da " + Rules.minPlayer + " a " + Rules.maxPlayer);
                 numPlayers = Convert.ToInt32(Console.ReadLine());
             }
-            Rules.numPlayer = numPlayers;
+            Rules.SetnumPlayer(numPlayers);
         }
         public static void howToPlay()
         {
@@ -59,11 +60,11 @@ namespace GIocodellaSigaretta
             Game Rules = new Game();
             string namePlayer;
             Console.WriteLine("OK inserite i vostri nomi");
-            for(int i = 0; i < Rules.numPlayer; i ++)
+            for(int i = 0; i < Rules.GetnumPlayer(); i ++)
             {
                 Console.WriteLine("OK tocca al " + i + "* tra di voi");
                 namePlayer = Console.ReadLine();
-                Rules.namePlayers.Add(namePlayer);
+                Rules.SetnamePlayers(namePlayer);
             }
         }
         public static void listQuestionTakeAnswer ()
@@ -76,24 +77,64 @@ namespace GIocodellaSigaretta
             {
                 int j = rand.Next(i, gameContent.allQuestions.Length);
                 Console.WriteLine(gameContent.allQuestions[j]);
-                Console.WriteLine("risponde " + Rules.namePlayers);
+                Console.WriteLine("risponde " + Rules.GetnamePlayers());
                 answerPlayer = Console.ReadLine();
-                gameContent.allAnswers.Add(answerPlayer);
+                gameContent.SetallAnswer(answerPlayer);
                 Console.Clear();
             }
+        }
+    }
+    class Player
+    {
+        List<string> PlayersNames = new List<string>();
+        List<string> AllTheAnswers = new List<string>();
+        public Player (string PlayerName, string Answer)
+        {
+            PlayersNames.Add(PlayerName);
+            AllTheAnswers.Add(Answer);
         }
     }
     public class Contents
     {
         public string[] allQuestions = new string[15] { "Chi?", "Con Chi?", "Cosa fanno?", "Dove?", "Perchè?", "Ma sopra o sotto?", "Chi ha vinto?", "Chi ha perso?", "Infine?", "Ma perchè proprio sta cosa?", "Chi lo ha fatto a chi?", "Sì, ma perche?", "Chi lo ha fatto con Silvio Berlusconi?", "Come l'ha presa?", "Chi e' stato con Maria De Filippi?" };
-        public List<string> allAnswers = new List<string>();
+        private List<string> allAnswers = new List<string>();
+        public void SetallAnswer (string Answers)
+        {
+            allAnswers.Add(Answers);
+        }
     }
     public class Game
     {
         public int minPlayer = 2;
         public int maxPlayer = 12;
         public int deadlineNumPlayer = 20;
-        public int numPlayer;
-        public List<string> namePlayers = new List<string>();
+        private int numPlayer;
+        private List<string> namePlayers = new List<string>();
+        //creazione metodi per i campi
+        //i campi devono poter viaggiare senza alcun intoppo 
+        //prevedo, quindi l'uso di 2 metodi per ciascun campo
+        //un get e set per questi campi importanti
+
+        //metodi campo numPlayer
+        public int GetnumPlayer()
+        {
+            return numPlayer;
+        }
+
+        public void SetnumPlayer(int numPlayers)
+        {
+            numPlayer = numPlayers;
+        }
+
+        //metodi campo namePlayers
+        public List<string> GetnamePlayers()
+        {
+            return namePlayers;
+        }
+
+        public void SetnamePlayers(string Names)
+        {
+            namePlayers.Add(Names);
+        }
     }
 }
